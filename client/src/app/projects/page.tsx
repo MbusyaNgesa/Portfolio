354d8de90e4deee24eafdd99fdcba8cd795c2390
MbusyaNgesa <theengesa@gmail.com>
2025-01-16 02:23:26 +0300

    Links to projects

--- client/src/app/projects/page.tsx
+++ client/src/app/projects/page.tsx
@@ -1,6 +1,7 @@
-import { Card, CardContent, CardHeader } from "@mui/material";
+import { Button, Card, CardContent, CardHeader } from "@mui/material";
 import project2 from "../images/pfp2.jpeg";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Projects() {
   const projects = [
@@ -11,6 +12,7 @@ export default function Projects() {
         "A web-based events listing platform that incorporates vendors, a ticket system, and two admin dashboards.",
       ],
       techStack: ["NextJS", "TypeScript", "Express", "NodeJS"],
+      link: "https://github.com/MbusyaNgesa/eventnami",
     },
     {
       image: project2,
@@ -19,6 +21,7 @@ export default function Projects() {
         "A web-based events listing platform that incorporates vendors, a ticket system, and two admin dashboards.",
       ],
       techStack: ["NextJS", "TypeScript", "Express", "NodeJS"],
+      link: "https://github.com/MbusyaNgesa/eventnami",
     },
     {
       image: project2,
@@ -27,25 +30,31 @@ export default function Projects() {
         "A web-based events listing platform that incorporates vendors, a ticket system, and two admin dashboards.",
       ],
       techStack: ["NextJS", "TypeScript", "Express", "NodeJS"],
+      link: "https://github.com/MbusyaNgesa/eventnami",
     },
   ];
 
   return (
-    <div className="min-h-screen bg-[#0a0a1a] text-white py-20">
+    <div className="min-h-screen py-10">
       <div className="max-w-6xl mx-auto px-4">
         <div className="text-center mb-16">
           <h1 className="text-4xl font-bold mb-4">PROJECTS</h1>
-          <p className="text-gray-400 text-lg">
+          <div className="border-b-2 border-gray-300 mb-6"></div>
+          <p className="text-gray-600 text-lg">
             A few of the projects that I have worked on over the years that I
             can publicly talk about.
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-4xl mx-auto">
+        <div
+          className="grid grid-cols-1 md:grid-cols-2 gap-8 
+        max-w-4xl mx-auto"
+        >
           {projects.map((project, index) => (
             <Card
               key={index}
-              className="bg-gray-800 border-gray-700 overflow-hidden"
+              className="bg-gray-800 border-gray-700 
+              overflow-hidden hover:scale-105 hover:cursor-pointer "
             >
               <div className="relative h-48 w-full">
                 <Image
@@ -71,6 +80,12 @@ export default function Projects() {
                   ))}
                 </div>
                 {/* <div className="text-gray-400">{project.year}</div> */}
+                <div>
+                  <Button>
+                    <Link href={project.link}>Github</Link>
+                  </Button>
+                  <Button>Live</Button>
+                </div>
               </CardContent>
             </Card>
           ))}
@@ -79,3 +94,55 @@ export default function Projects() {
     </div>
   );
 }
+
+// import { Card } from "@/types";
+// import { ProjectCard } from "@/components/ProjectCard";
+// import project from "../images/pfp2.jpeg";
+// import project2 from "../images/icon2.jpg";
+
+// export default function Projects() {
+//   const projectData: Card[] = [
+//     {
+//       image: project,
+//       title: "Eventlify",
+//       description: [
+//         "A web-based events listing platform that incorporates vendors, a ticket system, and two admin dashboards.",
+//       ],
+//       techStack: ["NextJS", "TypeScript", "Express", "NodeJS"],
+//     },
+//     {
+//       image: project2,
+//       title: "Eventlify",
+//       description: [
+//         "A web-based events listing platform that incorporates vendors, a ticket system, and two admin dashboards.",
+//       ],
+//       techStack: ["NextJS", "TypeScript", "Express", "NodeJS"],
+//     },
+//     {
+//       image: project,
+//       title: "Eventlify",
+//       description: [
+//         "A web-based events listing platform that incorporates vendors, a ticket system, and two admin dashboards.",
+//       ],
+//       techStack: ["NextJS", "TypeScript", "Express", "NodeJS"],
+//     },
+//   ];
+
+//   return (
+//     <>
+//       <div>
+//         <div className="text-center my-6">
+//           <h1 className="text-2xl font-bold">Projects</h1>
+//           <div className="border-b-2 border-gray-300 w-20 mx-auto my-2"></div>
+//         </div>
+//         <div className="flex justify-center">
+//           <div className="grid md:grid-cols-2 grid-cols-1 gap-y-4 gap-x-6">
+//             {projectData.map((project, index) => (
+//               <ProjectCard key={index} card={project} />
+//             ))}
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
