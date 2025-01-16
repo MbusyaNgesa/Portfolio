800c8b0bc9e91af36a0e93c0df2798af1bf8f678
MbusyaNgesa <theengesa@gmail.com>
2025-01-17 02:25:30 +0300

    Error solving

--- client/src/app/projects/page.tsx
+++ client/src/app/projects/page.tsx
@@ -51,46 +51,44 @@ export default function Projects() {
         max-w-4xl mx-auto"
         >
           {projects.map((project, index) => (
-            <Link href={project.link}>
-              <Card
-                key={index}
-                className="bg-gray-800 border-gray-700 
+            <Card
+              key={index}
+              className="bg-gray-800 border-gray-700 
               overflow-hidden hover:scale-105 hover:cursor-pointer 
               transition duration-700 "
-              >
-                <div className="relative h-48 w-full">
-                  <Image
-                    src={project.image || "No Project Image"}
-                    alt={`${project.title} preview`}
-                    fill
-                    className="object-cover"
-                  />
+            >
+              <div className="relative h-48 w-full">
+                <Image
+                  src={project.image || "No Project Image"}
+                  alt={`${project.title} preview`}
+                  fill
+                  className="object-cover"
+                />
+              </div>
+              <CardHeader>
+                <h2 className="text-xl font-bold">{project.title}</h2>
+              </CardHeader>
+              <CardContent>
+                <p className="text-gray-300 mb-4">{project.description}</p>
+                <div className="flex flex-wrap gap-2 mb-4">
+                  {project.techStack.map((stack, stackIndex) => (
+                    <span
+                      key={stackIndex}
+                      className="px-3 py-1 bg-gray-700 text-gray-300 text-sm rounded"
+                    >
+                      {stack}
+                    </span>
+                  ))}
                 </div>
-                <CardHeader>
-                  <h2 className="text-xl font-bold">{project.title}</h2>
-                </CardHeader>
-                <CardContent>
-                  <p className="text-gray-300 mb-4">{project.description}</p>
-                  <div className="flex flex-wrap gap-2 mb-4">
-                    {project.techStack.map((stack, stackIndex) => (
-                      <span
-                        key={stackIndex}
-                        className="px-3 py-1 bg-gray-700 text-gray-300 text-sm rounded"
-                      >
-                        {stack}
-                      </span>
-                    ))}
-                  </div>
-                  {/* <div className="text-gray-400">{project.year}</div> */}
-                  <div>
-                    <Button>
-                      {/* <Link href={project.link}>Github</Link> */}
-                    </Button>
-                    <Button>Live</Button>
-                  </div>
-                </CardContent>
-              </Card>
-            </Link>
+                {/* <div className="text-gray-400">{project.year}</div> */}
+                <div>
+                  <Button>
+                    <Link href={project.link}>Github</Link>
+                  </Button>
+                  <Button>Live</Button>
+                </div>
+              </CardContent>
+            </Card>
           ))}
         </div>
       </div>
