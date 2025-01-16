f262523ba99f8d924ba0ce8d506460df6d63ccce
MbusyaNgesa <theengesa@gmail.com>
2025-01-17 01:58:47 +0300

    Error Solving

--- client/src/app/projects/page.tsx
+++ client/src/app/projects/page.tsx
@@ -12,7 +12,7 @@ export default function Projects() {
         "A web-based events listing platform that incorporates vendors, a ticket system, and two admin dashboards.",
       ],
       techStack: ["NextJS", "TypeScript", "Express", "NodeJS"],
-      link: "https://github.com/MbusyaNgesa/eventnami",
+      link: "https://github.com/MbusyaNgesa/eventlify",
     },
     {
       image: project2,
@@ -51,44 +51,46 @@ export default function Projects() {
         max-w-4xl mx-auto"
         >
           {projects.map((project, index) => (
-            <Card
-              key={index}
-              className="bg-gray-800 border-gray-700 
+            <Link href={project.link}>
+              <Card
+                key={index}
+                className="bg-gray-800 border-gray-700 
               overflow-hidden hover:scale-105 hover:cursor-pointer 
               transition duration-700 "
-            >
-              <div className="relative h-48 w-full">
-                <Image
-                  src={project.image || "No Project Image"}
-                  alt={`${project.title} preview`}
-                  fill
-                  className="object-cover"
-                />
-              </div>
-              <CardHeader>
-                <h2 className="text-xl font-bold">{project.title}</h2>
-              </CardHeader>
-              <CardContent>
-                <p className="text-gray-300 mb-4">{project.description}</p>
-                <div className="flex flex-wrap gap-2 mb-4">
-                  {project.techStack.map((stack, stackIndex) => (
-                    <span
-                      key={stackIndex}
-                      className="px-3 py-1 bg-gray-700 text-gray-300 text-sm rounded"
-                    >
-                      {stack}
-                    </span>
-                  ))}
+              >
+                <div className="relative h-48 w-full">
+                  <Image
+                    src={project.image || "No Project Image"}
+                    alt={`${project.title} preview`}
+                    fill
+                    className="object-cover"
+                  />
                 </div>
-                {/* <div className="text-gray-400">{project.year}</div> */}
-                <div>
-                  <Button>
-                    <Link href={project.link}>Github</Link>
-                  </Button>
-                  <Button>Live</Button>
-                </div>
-              </CardContent>
-            </Card>
+                <CardHeader>
+                  <h2 className="text-xl font-bold">{project.title}</h2>
+                </CardHeader>
+                <CardContent>
+                  <p className="text-gray-300 mb-4">{project.description}</p>
+                  <div className="flex flex-wrap gap-2 mb-4">
+                    {project.techStack.map((stack, stackIndex) => (
+                      <span
+                        key={stackIndex}
+                        className="px-3 py-1 bg-gray-700 text-gray-300 text-sm rounded"
+                      >
+                        {stack}
+                      </span>
+                    ))}
+                  </div>
+                  {/* <div className="text-gray-400">{project.year}</div> */}
+                  <div>
+                    <Button>
+                      {/* <Link href={project.link}>Github</Link> */}
+                    </Button>
+                    <Button>Live</Button>
+                  </div>
+                </CardContent>
+              </Card>
+            </Link>
           ))}
         </div>
       </div>
